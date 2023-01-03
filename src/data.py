1fbf6433a3c17ad833635ef103070639fbbaf62f
Marcio Lima <marcio.lima.inacio@gmail.com>
2023-01-03 13:16:56 +0000

    Add NER to the preprocessing pipeline

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import NLPyPort as pyport
@@ -21,12 +22,26 @@ def preprocess_data(corpus: pd.DataFrame) -> pd.DataFrame:
     def lemmatizer(sent):
         return pyport.lematizador_normal(sent['Tokens'], sent['POS Tags'])
 
+    def ner(sent, model):
+        X = list()
+        for i in range(len(sent['Tokens'])):
+            feats = pyport.features(sent['Tokens'], sent['POS Tags'],
+                                    sent['Lemma'], i, [True, True])
+            X.append(feats)
+        prediction = model.predict([X])
+        return prediction[0]
+
     logger.info('Tokenizing corpus')
     corpus['Tokens'] = corpus['Text'].apply(tokenizer)
     logger.info('Tagging corpus')
     corpus['POS Tags'] = corpus['Tokens'].apply(tagger)
     logger.info('Lemmatizing corpus')
     corpus['Lemma'] = corpus.apply(lemmatizer, axis=1)
+    logger.info('Running Named Entity Recognition')
+    pyport_path = Path(os.path.dirname(pyport.__file__))
+    model_path = pyport_path / 'CRF/trainedModels/harem.pickle'
+    model = pyport.load_model(model_path)
+    corpus['NER'] = corpus.apply(ner, axis=1, args=(model,))
 
-    logger.info('Preprocessing done')
+    logger.debug(f'Preprocessed corpus\n\n{corpus}')
     return corpus

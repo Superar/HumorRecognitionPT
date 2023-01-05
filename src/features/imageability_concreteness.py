fb3d4744b3e3926a75acb5c16b0e48330a57f8f4
Marcio Lima <marcio.lima.inacio@gmail.com>
2023-01-05 13:46:02 +0000

    Fix typo

--- src/features/imageability_concreteness.py
+++ src/features/imageability_concreteness.py
@@ -17,9 +17,9 @@ def calculate_values(lemmas: list[str], mwp: pd.DataFrame):
 
 def calculate_imageability_concreteness(corpus: pd.DataFrame,
                                         mwp: Path) -> pd.DataFrame:
-    logger.info('Reading Minho World Pool lexicon')
+    logger.info('Reading Minho Word Pool lexicon')
     mwp = pd.read_json(mwp)
-    logger.debug(f'Minho World Pool\n\n{mwp}')
+    logger.debug(f'Minho Word Pool\n\n{mwp}')
 
     logger.info('Calculating imageability and concreteness features')
     values = corpus['Lemma'].apply(calculate_values, mwp=mwp).fillna(0)

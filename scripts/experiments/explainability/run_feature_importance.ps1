890f6844b787b8aed5cd8e92a9dd55779a57361c
Marcio Lima <marcio.lima.inacio@gmail.com>
2023-02-07 14:20:09 +0000

    Feature importances with fixed features

--- scripts/experiments/explainability/run_feature_importance.ps1
+++ scripts/experiments/explainability/run_feature_importance.ps1
@@ -1,8 +1,8 @@
 # Content features
-# python .\scripts\experiments\explainability\feature_importance.py `
-#     .\data\Humor\features\all_content_features\test\data.hdf5 `
-#     .\results\models\all_content_features\SVCLinear\ `
-#     .\results\explainability\all_content_features_SVCLinear.ods
+python .\scripts\experiments\explainability\feature_importance.py `
+    .\data\Humor\features\all_content_features\test\data.hdf5 `
+    .\results\models\all_content_features\SVCLinear\ `
+    .\results\explainability\all_content_features_SVCLinear.ods
 
 # Humor features
 python .\scripts\experiments\explainability\feature_importance.py `
@@ -11,7 +11,7 @@ python .\scripts\experiments\explainability\feature_importance.py `
     .\results\explainability\all_humor_features_RandomForest.ods
 
 # All features
-# python .\scripts\experiments\explainability\feature_importance.py `
-#     .\data\Humor\features\all_all_features\test\data.hdf5 `
-#     .\results\models\all_all_features\RandomForest\ `
-#     .\results\explainability\all_all_features_RandomForest.ods
+python .\scripts\experiments\explainability\feature_importance.py `
+    .\data\Humor\features\all_all_features\test\data.hdf5 `
+    .\results\models\all_all_features\RandomForest\ `
+    .\results\explainability\all_all_features_RandomForest.ods
